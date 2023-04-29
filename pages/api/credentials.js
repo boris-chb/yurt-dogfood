ef5d7fd513a01823d6d30fa0596ab7617069aad3
boris-chb <47457848+boris-chb@users.noreply.github.com>
2023-04-29 21:10:14 +0100

    1 file modified

--- pages/api/credentials.js
+++ pages/api/credentials.js
@@ -1,17 +1,34 @@
-import cors from 'cors';
+import bodyParser from 'body-parser';
+import Cors from 'cors';
 
-// Set up CORS middleware
-const corsMiddleware = cors({
-  origin: '*', // Allow any origin to access the resource
-  methods: ['POST', 'GET'], // Allow only POST requests
-});
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['POST', 'OPTIONS'], // Allow only POST and OPTIONS requests
+  })
+);
+
+const jsonParser = bodyParser.json();
 
 export default function handler(req, res) {
-  corsMiddleware(req, res, async () => {
+  await cors(req, res);
+
+  jsonParser(req, res, async () => {
     if (req.method === 'POST') {
       const allowedUsers = ['bciobirca'];
-      let body = await req.json();
-      console.log(body);
+      const data = req.body;
+      console.log(data);
 
       res.status(200).json({ message: 'POST success' });
     }
