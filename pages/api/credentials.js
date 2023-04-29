02bd371820a88a0822046b0fb9d284885f552099
boris-chb <47457848+boris-chb@users.noreply.github.com>
2023-04-29 21:48:22 +0100

    1 file modified

--- pages/api/credentials.js
+++ pages/api/credentials.js
@@ -1,25 +1,8 @@
-import bodyParser from 'body-parser';
 import Cors from 'cors';
 
-function initMiddleware(middleware) {
-  return (req, res) =>
-    new Promise((resolve, reject) => {
-      middleware(req, res, (result) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-        return resolve(result);
-      });
-    });
-}
-
-const cors = initMiddleware(
-  Cors({
-    methods: ['POST', 'OPTIONS'], // Allow only POST and OPTIONS requests
-  })
-);
-
-const jsonParser = bodyParser.json();
+const cors = Cors({
+  methods: ['POST', 'OPTIONS'], // Allow only POST and OPTIONS requests
+});
 
 export default async function handler(req, res) {
   await cors(req, res);
@@ -28,28 +11,22 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  jsonParser(req, res, async () => {
-    if (req.method === 'POST') {
-      const allowedUsers = ['bciobirca'];
+  if (req.method === 'POST') {
+    const allowedUsers = ['bciobirca'];
 
-      console.log(req.body);
-      console.log(typeof req.body);
-      console.log(req.body.user);
-      return res.status(200);
-      // const data = JSON.parse(req.body);
-      // console.log(data);
+    const data = await req.json();
+    console.log(data);
 
-      // if (!allowedUsers.includes(data.user)) {
-      //   return res.status(401).json({ allowed: false });
-      // }
-
-      // return res.status(200).json({ allowed: true });
+    if (!allowedUsers.includes(data.user)) {
+      return res.status(401).json({ allowed: false });
     }
 
-    if (req.method === 'GET') {
-      res.status(200).json({ allowed: true });
-    }
-  });
+    return res.status(200).json({ allowed: true });
+  }
 
-  return new Response('all good', { status: 200 });
+  if (req.method === 'GET') {
+    return res.status(200).json({ allowed: true });
+  }
+
+  return res.status(405).json({ message: 'Method Not Allowed' });
 }
