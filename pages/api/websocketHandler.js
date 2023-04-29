fbeb3f55b8561d1c7695255f2956c141c7b8c40b
boris-chb <47457848+boris-chb@users.noreply.github.com>
2023-04-29 18:41:38 +0100

    1 file modified

--- pages/api/websocketHandler.js
+++ pages/api/websocketHandler.js
@@ -24,39 +24,28 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
-  if (req.method === 'POST') {
-    // Perform WebSocket handshake
-
-    try {
-    } catch (e) {
-      console.error(error);
-    }
-    const io = new Server();
-    const socket = await new Promise((resolve) => {
-      io.on('connection', (socket) => {
-        resolve(socket);
-      });
-      io.attach(req.socket);
+  const io = new Server();
+  const socket = await new Promise((resolve) => {
+    io.on('connection', (socket) => {
+      resolve(socket);
     });
+    io.attach(req.socket);
+  });
 
-    // Handle WebSocket messages
-    socket.on('message', (message) => {
-      console.log(`Received message from client: ${message}`);
-      // Send a message back to the client
-      socket.emit('message', `Hello, client! You said: ${message}`);
-    });
+  // Handle WebSocket messages
+  socket.on('message', (message) => {
+    console.log(`Received message from client: ${message}`);
+    // Send a message back to the client
+    socket.emit('message', `Hello, client! You said: ${message}`);
+  });
 
-    // Handle WebSocket errors
-    socket.on('error', (error) => {
-      console.error(`WebSocket error: ${error}`);
-    });
+  // Handle WebSocket errors
+  socket.on('error', (error) => {
+    console.error(`WebSocket error: ${error}`);
+  });
 
-    // Handle WebSocket close
-    socket.on('close', () => {
-      console.log(`WebSocket connection closed`);
-    });
-  } else {
-    res.send('');
-    res.status(405).end();
-  }
+  // Handle WebSocket close
+  socket.on('close', () => {
+    console.log(`WebSocket connection closed`);
+  });
 }
