3a7aa3d58a6f8da7f99f15286e30ce910c7d3851
boris-chb <47457848+boris-chb@users.noreply.github.com>
2023-04-29 20:37:19 +0100

    3 files modified

--- pages/api/new-item.js
+++ pages/api/new-item.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST')
     return new Response('Only post requests', { status: 400 });
 
