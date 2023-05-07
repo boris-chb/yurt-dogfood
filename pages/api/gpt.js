b88d208444f664c44426627204da02205461457e
Boris <47457848+boris-chb@users.noreply.github.com>
2023-05-07 14:55:56 +0100

    wtf

--- pages/api/gpt.js
+++ pages/api/gpt.js
@@ -1,7 +1,5 @@
 import Cors from 'cors';
 
-// Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
 });
